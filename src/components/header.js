2e2294dde394cbc362dfdbbc2305ad8219c7b796
xRaynii <60364920+xRaynii@users.noreply.github.com>
2020-11-07 11:50:18 -0400

    Estilos añadidos en header y navegacion

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { css } from '@emotion/core';
 
 import Navegacion from './navegacion';
 
 const Header = () => {
 
     return ( 
-        <header>
-            <div>
+        <header
+            css={css`
+                background-color: #0D283B;
+                padding: 1rem;
+            `}
+        >
+            <div
+                css={css`
+                    max-width: 1200px;
+                    margin: 0 auto;
+                    text-align: center;
+
+                    @media(min-width: 768px){
+                        display: flex;
+                        align-items: center;
+                        justify-content: space-between;
+                    }
+                `}
+            >
                 <Link to={"/"}>
                     Bienes Raices
                 </Link>
